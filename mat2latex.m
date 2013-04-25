119e92d1afc539d83263cbbb411535923db46e73
Wojciech Waśko <wojciech.wasko@gmail.com>
2013-04-25 09:38:16 +0200

    Migration to Matlab's built-in matrix operations.
    theoretically, it should be faster

--- mat2latex.m
+++ mat2latex.m
@@ -1,30 +1,9 @@
-function [] = mat2latex(a)
-    % This function converts a numerical Matlab matrix into its LaTeX 
-    % counterpart and prints that matrix. 
-    % input:
-    % a - numerical matrix
-    nrows = size(a, 1);
-    ncols = size(a, 2);
-    for i = 1:nrows
-        for j = 1:ncols
-            val = a(i,j);
-            if abs(val) < 1e-10
-                val = 0;
-            end
-            if j == ncols
-                if i == nrows
-                    fprintf(1, ' %s', num2str(val, 4));
-                else
-                    fprintf(1, ' %s \\\\', num2str(val, 4));
-                end
-            else
-                fprintf(1, ' %s &', num2str(val, 4));
-            end
-        end
-        if ncols > 1
-            fprintf(1, '\n');
-        end
-    end
-    fprintf(1, '\n');
+function [s] = mat2latex(a)
+    fmt = '%g';
+    [nr,nc] = size(a);
+    row = [fmt, ' ', repmat(['& ', fmt, ' '], 1, nc-1)];
+    m = [repmat([row, '\\\\ \n'], 1, nr-1), [row, '\n']];
+    ac = num2cell(reshape(a',1,[]));
+    % print values
+    s = sprintf(m,ac{:});
 end
-
