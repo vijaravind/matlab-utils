1306aaf890d6f2ebaa9ac8c9832e6d93c6b256e6
Wojciech Waśko <wojciech.wasko@gmail.com>
2013-04-25 09:40:46 +0200

    Removing unneccessary comment.

--- mat2latex.m
+++ mat2latex.m
@@ -4,6 +4,5 @@ function [s] = mat2latex(a)
     row = [fmt, ' ', repmat(['& ', fmt, ' '], 1, nc-1)];
     m = [repmat([row, '\\\\ \n'], 1, nr-1), [row, '\n']];
     ac = num2cell(reshape(a',1,[]));
-    % print values
     s = sprintf(m,ac{:});
 end
